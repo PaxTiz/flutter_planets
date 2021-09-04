2c7e704b5a6c615cf079757451542dbaaddb9991
Valentin Cernuta <vcernuta3@gmail.com>
2021-09-04 15:56:45 +0200

    Use Hero animations when switching to planet details screen

--- lib/components/planets/planet_list_item.dart
+++ lib/components/planets/planet_list_item.dart
@@ -8,31 +8,38 @@ class PlanetListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context).pushNamed('/planet-details', arguments: planet);
-      },
-      child: Container(
-        width: 200,
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardTheme.color,
-          borderRadius: BorderRadius.circular(16),
-        ),
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
+    return Hero(
+      tag: planet.id,
+      transitionOnUserGestures: true,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            Navigator.of(context).pushNamed('/planet-details', arguments: planet);
+          },
+          child: Container(
+            width: 200,
+            decoration: BoxDecoration(
+              color: Theme.of(context).cardTheme.color,
               borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                planet.image,
-                height: 250,
-                fit: BoxFit.cover,
-              ),
             ),
-            SizedBox(height: 16),
-            Text(planet.name)
-          ],
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: Image.network(
+                    planet.image,
+                    height: 250,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                SizedBox(height: 16),
+                Text(planet.name)
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/screens/planets/planet_details.dart
+++ lib/screens/planets/planet_details.dart
@@ -3,57 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:planets/models/planet.dart';
 
 class PlanetDetails extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     final planet = ModalRoute.of(context)?.settings.arguments as Planet;
     return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          Icon(CupertinoIcons.ellipsis),
+          SizedBox(width: 16),
+        ],
+        leading: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: Icon(CupertinoIcons.arrow_left, color: Colors.white)
+        ),
+        actionsIconTheme: IconThemeData(color: Colors.white),
+      ),
       body: SingleChildScrollView(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.of(context).pop(),
-                  child: Icon(CupertinoIcons.arrow_left, color: Colors.white),
-                ),
-                Icon(CupertinoIcons.ellipsis, color: Colors.white),
-              ],
+            Hero(
+              tag: planet.id, 
+              child: Image.network(
+                planet.image,
+                fit: BoxFit.cover,
+              ),
             ),
-            SizedBox(height: 16),
-            Image.network(
-              planet.image,
-              fit: BoxFit.cover,
+            SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    planet.name,
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 8),
+                  Divider(color: Colors.white30),
+                  SizedBox(height: 8),
+                  Text(
+                    planet.description,
+                    textAlign: TextAlign.justify,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline5
+                        ?.copyWith(height: 1.6, fontSize: 16),
+                  ),
+                  SizedBox(height: 8),
+                  Divider(color: Colors.white30),
+                  SizedBox(height: 8),
+                  Text(
+                    "Gallery",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline6
+                        ?.copyWith(fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  SizedBox(height: 16),
+                ],
+              ),
             ),
-            SizedBox(height: 16),
-            Text(
-                planet.name,
-              style: Theme.of(context).textTheme.headline6,
-            ),
-
-            SizedBox(height: 8),
-            Divider(color: Colors.white30),
-            SizedBox(height: 8),
-
-            Text(
-              planet.description,
-              style: Theme.of(context).textTheme.headline5?.copyWith(height: 1.6),
-            ),
-
-            SizedBox(height: 8),
-            Divider(color: Colors.white30),
-            SizedBox(height: 8),
-
-            Text(
-              "Gallery",
-              style: Theme.of(context).textTheme.headline6?.copyWith(fontSize: 18),
-            ),
-            SizedBox(height: 8),
-            SizedBox(height: 16),
           ],
         ),
       ),
