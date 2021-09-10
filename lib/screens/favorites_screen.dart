f2649b769c4cfe094b725396c7bd11d18bffcda7
Valentin Cernuta <vcernuta3@gmail.com>
2021-09-10 21:31:30 +0200

    Clean imports

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:planets/components/header.dart';
-import 'package:planets/components/planets/most_popular_carousel.dart';
 
 class FavoritesScreen extends StatelessWidget {
   @override
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:planets/components/header.dart';
-import 'package:planets/components/planets/most_popular_carousel.dart';
-
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
