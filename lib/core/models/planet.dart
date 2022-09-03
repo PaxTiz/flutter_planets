5f223d3257ed966094bc14e5fba22a2540906430
Valentin Cernuta <vcernuta3@gmail.com>
2022-09-03 15:55:26 +0200

    fix: planet model

--- lib/core/models/planet.dart
+++ lib/core/models/planet.dart
@@ -1,34 +1,22 @@
 class Planet {
-  final int id;
+  final String id;
   final String name;
   final String description;
   final String image;
-  final int distance;
-  final String distanceUnit;
-  final Map<String, double>? composition;
 
   const Planet({
     required this.id,
     required this.name,
     required this.description,
     required this.image,
-    required this.distance,
-    required this.distanceUnit,
-    required this.composition,
   });
 
   static Planet fromJson(Map<String, dynamic> json) {
     return Planet(
-      id: json['id'] as int,
+      id: json['id'] as String,
       name: json['name'] as String,
       description: json['description'] as String,
       image: json['image'] as String,
-      distance: json['distance'] as int,
-      distanceUnit: json['distance_unit'] as String,
-      composition: json['composition'] == null
-          ? null
-          : Map<String, double>.from(
-              json['composition'] as Map<String, dynamic>),
     );
   }
 
